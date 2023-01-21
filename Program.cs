d8ecd027a4c6827637fcaee3824c39d5675b2671
Harytamo <107985314+Anybis91@users.noreply.github.com>
2023-01-21 19:53:06 +0300

    ReadInt

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task_Readint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int enterNumber=ReadInt();
+        }
+
+        static int ReadInt()
+        {
+            bool isWork = true;
+            Console.Write("Введите число для преобразования:");
+            int enterNumber=0;
+
+            while (isWork)
+            {
+                string userInput = Console.ReadLine();
+                int.TryParse(userInput, out enterNumber);
+                bool result = int.TryParse(userInput, out enterNumber);
+
+                if (result == false)
+                {
+                    Console.WriteLine("Число не удалось преобразовать. Введите корректное число!");
+                    userInput = Console.ReadLine();
+                }
+                else
+                {
+                    isWork = false;
+                }
+            }
+           
+            return enterNumber;
+        }
+    }
+}
